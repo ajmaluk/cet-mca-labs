7d9d230013205fca16e14f1433eb42ac07532d91
ajmal-uk <ajmaluk.me@gmail.com>
2026-02-11 16:50:02 +0530

    ...............................................................

---
+++ sem2/oop/trail/SearchArray.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class ArraySearch {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] array = new int[];
+    }
+}
